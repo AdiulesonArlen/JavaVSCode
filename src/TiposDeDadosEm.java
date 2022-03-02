1592f28988172336f29f8d23c19ff66e18a43d8f
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2022-03-01 23:07:49 -0300

    Renomeacao do arquivo
    Renomeação do App.java para TiposDeDadosEm.java

---
+++ src/TiposDeDadosEm.java
@@ -0,0 +1,26 @@
+public class TiposDeDadosEm {
+    public static void main(String[] args) throws Exception {
+        //Variáveis do tipo Iinteiro:
+        byte numeroPequeno = 125;
+        short numeroShort = 32767;
+        int numeroMaior = 2147483647;
+        long numeroEnorme = 922337203685477000l;
+        
+        //Variáveis do tipo ponto Flutuante:
+        float peso = 78.7f;
+        double pi = 3.1484941648431184131518;
+
+        //Variáveis do tipo Caractere:
+        char letraOuSimbolo = '+';
+
+        //Variáveis do tipo Booleano:
+        boolean estouFaminto = false;
+
+        //Variáveis do tipo String:
+        String texto = "Olá Mundo!";
+
+        System.out.println(texto);
+        
+
+    }
+}
