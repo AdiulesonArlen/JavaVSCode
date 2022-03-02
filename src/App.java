ab090052d1b2118b86863c809ebedcca840e2375
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2022-03-01 22:52:06 -0300

    tipos de dados em Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte numeroPequeno = 125;
+        short numeroShort = 32767;
+
+        System.out.println(numeroShort);
+        System.out.println(numeroPequeno);
+        
+
     }
 }
